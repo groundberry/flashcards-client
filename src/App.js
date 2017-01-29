ae19242216bb433bb7cdcf9c2c6fe0713702dc90
Blanca Mendizabal Perello <blanca.mendizabal.perello@gmail.com>
2017-01-29 16:50:33 +0000

    Remove placeholder content

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,11 +6,10 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Flashcards</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          An app to learn any subject through spaced repetition.
         </p>
       </div>
     );
