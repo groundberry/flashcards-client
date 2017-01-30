978cbf5c1a69e26071ae09885476682d12cdc829
Blanca Mendizabal Perello <blanca.mendizabal.perello@gmail.com>
2017-01-30 19:06:31 +0000

    Create side bar for tags

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Tags from './tags.js';
 import './App.css';
 
 class App extends Component {
@@ -11,6 +12,7 @@ class App extends Component {
         <p className="App-intro">
           An app to learn any subject through spaced repetition.
         </p>
+        <Tags/>
       </div>
     );
   }
